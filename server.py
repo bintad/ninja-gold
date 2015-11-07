f24f7a321a4a29aca231a19b0926e5d18251b735
Jesse Naldo <jessenaldo@Jesses-MacBook-Pro.local>
2015-11-07 12:38:30 -0800

    reverted back to old code with multiple routes, created an array with appended messages, works properly now.

--- server.py
+++ server.py
@@ -9,70 +9,70 @@ def index():
 	try:
 		session['count']
 		session['just_gold']
-		session['farm']
-		session['cave']
-		session['house'] 
-		session['casino']
-
-
+		session['now'] 
+		session['message']
 	except:
-		session['farm'] = 0
-		session['cave'] = 0
-		session['house'] = 0
-		session['casino'] = 0
 		session['count'] = 0
 		session['just_gold'] = 0
+		session['now'] = 0
+		session['message'] = []
+	
 
+	return render_template('index.html', count=session['count'], message=session['message'])
 
-	return render_template('index.html')
+@app.route('/farm', methods=['POST'])
+def farm_gold():
+	session['just_gold'] = 0
 
-
-	# return render_template('index.html', count=session['count'], just_gold=session['just_gold'], now=session['now'], casino=session['casino'])
-
-# @app.route('/farm', methods=['POST'])
-# def farm_gold():
-# 	session['casino'] = 0
-# 	session['now'] = datetime.now()
-# 	session['just_gold'] += random.randint(10,20) 
-# 	session['count'] += session['just_gold']
-# 	return redirect('/')
-
-@app.route('/process_money', methods=['POST'])
-def process_gold():
 	session['now'] = datetime.now()
-
-
-	if request.form['farm'] ==  'gold_farming':
-		session['just_gold'] += random.randint(10,20)
-		session['count'] += session['just_gold']
-		print request.form['farm']
-		
-		session['just_gold'] = 0
-
-	if request.form['cave'] == 'gold_caving' :
-		session['just_gold'] += random.randint(5,10)
-		session['count'] += session['just_gold']
-
-		session['just_gold'] = 0
-
-	if request.form['gold_method'] == session['house']:
-		session['just_gold'] += random.randint(2,5)
-		session['count'] += session['just_gold']
-
-		session['just_gold'] = 0
-
-	if request.form['gold_method'] == 'gold_casinoing':
-		session['count'] += random.randrange(-50,50)
-		# if num_gold < 0:
-		# 	session['just_gold'] = 
-		# 	session['count'] -= session['just_gold']
-		# else:
-		# 	session['just_gold'] = num_gold
-		# 	session['count'] += session['just_gold']
-
-		session['just_gold'] = 0
-
+	session['just_gold'] += random.randint(10,20) 
+	session['count'] += session['just_gold']
+	session['message'].append('Earned %s gold from the farm! %s' % (session['just_gold'], session['now']))
 	return redirect('/')
 
-app.run(debug=True)
 
+@app.route('/cave', methods=['POST'])
+def cave_gold():
+	session['now'] = 0
+	
+	session['now'] = datetime.now()
+	session['just_gold'] = 0
+	session['just_gold'] = random.randint(5,10)
+	session['count'] += session['just_gold']
+	session['message'].append('Earned %s gold from the cave! %s' % (session['just_gold'], session['now']))
+	return redirect('/')
+
+@app.route('/house', methods=['POST'])
+def house_gold():
+
+	session['now'] = 0
+	session['now'] = datetime.now()
+	session['just_gold'] = 0
+	session['just_gold'] = random.randint(2,5)
+	session['count'] += session['just_gold'] 
+	session['message'].append('Earned %s gold from the house! %s' % (session['just_gold'], session['now']))
+	return redirect('/')
+
+@app.route('/casino', methods=['POST'])
+def casino_gold():
+
+	session['now'] = 0
+	session['now'] = datetime.now()
+	session['just_gold'] = 0
+	earn_or_take = 0
+	num_gold = 0
+	num_gold = random.randint(0,50)
+	earn_or_take = random.randint(0,1)
+	if earn_or_take == 0:
+		session['just_gold'] -= num_gold
+		session['message'].append('Entered a casino and lost %s gold! %s' % (session['just_gold'], session['now']))
+	else:
+		session['just_gold'] += num_gold
+		session['message'].append('Entered a casino and gained %s gold! %s' % (session['just_gold'], session['now']))
+	session['count'] += session['just_gold']
+	return redirect('/')
+
+
+
+
+app.run(debug=True) 
