23672a6d99f2ec6d76f3ae14d9ddbfe1f0c45ae3
Jesse Naldo <jessenaldo@Jesses-MacBook-Pro.local>
2015-11-06 17:14:34 -0800

    did a whole bunch of stuff, basically.

--- server.py
+++ server.py
@@ -6,58 +6,74 @@ app.secret_key="charles_barkley"
 def index():
 	try:
 		session['count']
-		session['just_gold'] 
+		session['just_gold']
+		session['now'] 
+		session['casino']
 	except:
 		session['count'] = 0
 		session['just_gold'] = 0
+		session['now'] = 0
+		session['casino'] = 0
 	
 
-	return render_template('index.html', count=session['count'], just_gold=session['just_gold'] )
+	return render_template('index.html', count=session['count'], just_gold=session['just_gold'], now=session['now'], casino=session['casino'])
 
 @app.route('/farm', methods=['POST'])
 def farm_gold():
+	session['now'] = 0
+	session['casino'] = 0
+	from datetime import datetime
+	session['now'] = datetime.now()
 	session['just_gold'] = 0
-	x = [10,20]
 	import random
-	session['just_gold'] += random.randrange(x[0],x[1]) 
+	session['just_gold'] += random.randint(10,20) 
 	session['count'] += session['just_gold']
 	return redirect('/')
 
 
 @app.route('/cave', methods=['POST'])
 def cave_gold():
+	session['now'] = 0
+	session['casino'] = 0
+	from datetime import datetime
+	session['now'] = datetime.now()
 	session['just_gold'] = 0
-	x = [5,10]
 	import random
-	session['just_gold'] = random.randrange(x[0],x[1])
+	session['just_gold'] = random.randint(5,10)
 	session['count'] += session['just_gold']
 	return redirect('/')
 
 @app.route('/house', methods=['POST'])
 def house_gold():
+	session['casino'] = 0
+	session['now'] = 0
+	from datetime import datetime
+	session['now'] = datetime.now()
 	session['just_gold'] = 0
-	x = [2,5]
 	import random
-	session['just_gold'] = random.randrange(x[0],x[1])
+	session['just_gold'] = random.randint(2,5)
 	session['count'] += session['just_gold'] 
 	return redirect('/')
 
 @app.route('/casino', methods=['POST'])
 def casino_gold():
+	session['casino'] = 1
+	session['now'] = 0
+	from datetime import datetime
+	session['now'] = datetime.now()
 	session['just_gold'] = 0
-	x = [0,50]
-	y = [0,1]
 	earn_or_take = 0
 	num_gold = 0
 	import random
-	num_gold = random.randrange(x[0],x[1])
-	earn_or_take = random.randrange(y[0],y[1])
+	num_gold = random.randint(0,50)
+	earn_or_take = random.randint(0,1)
 	if earn_or_take == 0:
 		session['just_gold'] -= num_gold
 	else:
 		session['just_gold'] += num_gold
 	session['count'] += session['just_gold']
 	return redirect('/')
+
 
 
 app.run(debug=True) 
